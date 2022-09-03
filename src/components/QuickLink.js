26232462ca3fffd188da84d5a52f99c494f44d92
Andrew Keiser <webdev410@gmail.com>
2022-09-03 17:15:20 -0400

    show links btn

--- src/components/QuickLink.js
+++ src/components/QuickLink.js
@@ -1,26 +1,37 @@
 import React from "react";
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Button, Grid, Typography } from "@mui/material";
 import TEAMS from "./Teams/teams.json";
+import { useState } from "react";
 export default function QuickLink({}) {
+	const [showQuickLinks, setShowQuickLinks] = useState(true);
+	const toggleLinks = () => {
+		if (showQuickLinks) setShowQuickLinks(false);
+		else setShowQuickLinks(true);
+	};
 	return (
-		<Box sx={{ flexGrow: 1 }}>
-			<Grid container spacing={1}>
-				{TEAMS &&
-					TEAMS.map((team, i) => (
-						<>
-							<Grid item xs={1} key={`${team.shortName}${i}`}>
-								<a href={`#${team.shortName}`}>
-									<img
-										src={`${team.logo}`}
-										alt=""
-										width={"35px"}
-										height={"35px"}
-									/>
-								</a>
-							</Grid>
-						</>
-					))}
-			</Grid>
+		<Box sx={{}}>
+			<Button onClick={toggleLinks}>
+				{showQuickLinks ? "Hide Quick Links" : "Show Quick Links"}
+			</Button>
+			{showQuickLinks ? (
+				<Grid container spacing={1}>
+					{TEAMS &&
+						TEAMS.map((team, i) => (
+							<>
+								<Grid item xs={2} key={`${team.shortName}${i}`}>
+									<a href={`#${team.shortName}`}>
+										<img
+											src={`${team.logo}`}
+											alt=""
+											width={"35px"}
+											height={"35px"}
+										/>
+									</a>
+								</Grid>
+							</>
+						))}
+				</Grid>
+			) : null}
 		</Box>
 	);
 }
