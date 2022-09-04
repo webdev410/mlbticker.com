3c585c1bb874b107df8e90df98309f094a3df87f
Andrew Keiser <webdev410@gmail.com>
2022-09-04 14:43:42 -0400

    gtag

---
+++ src/components/Gtag.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+export default function Gtag() {
+	return (
+		<Box>
+			<Typography sx={{ color: "text.primary" }}>Gtag</Typography>
+		</Box>
+	);
+}
